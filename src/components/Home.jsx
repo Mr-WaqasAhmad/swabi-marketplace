29460e825c32af17cfc9f9e933cf6a9268ebb1ce
Waqas Ahmad <wa9580670@gmail.com>
2026-09-24 15:55:35 +0500

    Adjust main component margin-top

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -58,7 +58,7 @@ export const Home = () => {
         url="/"
       />
 
-      <main className='w-full min-h-screen pt-16 select-none bg-[#eee]'>
+      <main className='w-full min-h-screen mt-17 select-none bg-[#eee]'>
         {/* Search Banner */}
         <section
           className='max-w-6xl mx-auto px-3 sm:px-6 mt-2 sm:mt-4'
