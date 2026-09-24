d54ee4d1ac0e13b517045c79ffc429c5cea97b3f
Waqas Ahmad <wa9580670@gmail.com>
2026-09-24 15:36:51 +0500

    Remove 'My Profile' span from MobileNavLinks
    Remove 'My Profile' span from MobileNavLinks component.

--- src/components/MobileNavLinks.jsx
+++ src/components/MobileNavLinks.jsx
@@ -55,7 +55,6 @@ export const MobileNavLinks = ({ isOpened, setIsOpened }) => {
           <div className="w-10 h-10 rounded-full flex justify-center items-center text-xl bg-[#3b053d] font-semibold text-white">
             {firstNameLetter || 'U'}
           </div>
-          <span className="text-gray-800 font-semibold text-sm">My Profile</span>
         </Link>
       ) : (
         <Link
